d9dd417534018dd63ed398c16247d7a46a6f7262
devel <develope_e@naver.om>
2016-07-17 02:09:41 +0900

    알레르기, 제공업체 RESTless API 생성

--- app/restless/__init__.py
+++ app/restless/__init__.py
@@ -1,7 +1,9 @@
 from app.database import DBManager
-from app.model.user import User
+from app.model.user import User, Allergy, Provider
 from flask.ext.restless import APIManager
 
 def initRestlessApi(app):
     manager = APIManager(app, flask_sqlalchemy_db=DBManager.db)
     manager.create_api(User, methods=['GET'])
+    manager.create_api(Allergy, methods=['GET', 'PUT', 'DELETE'])
+    manager.create_api(Provider, methods=['GET', 'PUT', 'DELETE'])

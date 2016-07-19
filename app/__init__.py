27f0ccffe6a4f281186b75d54f85f09d86a10331
devel <develope_e@naver.om>
2016-07-20 04:47:48 +0900

    heroku fix 6

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,8 @@ def create_app():
 
     # Init routes
     from app.routes import controller
+    from app.routes import feedController
+    from app.routes import gcmController
 
     # Init Flask-restless
     from app.restless import initRestlessApi

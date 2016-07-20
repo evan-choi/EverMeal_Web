af629a5df0d6301446d739fc016db71a20838d90
devel <develope_e@naver.om>
2016-07-21 02:15:09 +0900

    db 초기화 api 추가

--- app/routes/controller.py
+++ app/routes/controller.py
@@ -4,6 +4,10 @@ import os
 from flask import send_file, request, jsonify
 
 from app.blueprint import basic
+from app.database import DBManager
+from app.model.article import Article
+from app.model.user import Gcm, Provider
+from core.Core import Allergy
 
 updateKey = "6041cef9600a531f527a69186b66bd21"
 
@@ -20,3 +24,21 @@ def download():
         return send_file(filename, as_attachment=True)
     else:
         return jsonify({"result": False})
+
+
+@basic.route('/task', methods=['DELETE'])
+def init():
+    if request.args.get("key") == updateKey:
+        try:
+            Allergy.query.delete()
+            Gcm.query.delete()
+            Provider.query.delete()
+            Article.query.delete()
+
+            DBManager.db.session.commit()
+
+            return jsonify({"result": True})
+        except:
+            DBManager.db.session.rollback()
+
+    return jsonify({"result": False})

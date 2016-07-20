daeb1dc2cfea5884833bd9ce0349c32300e7729b
devel <develope_e@naver.om>
2016-07-20 23:28:36 +0900

    re:heroku fix 1

--- core/Core.py
+++ core/Core.py
@@ -28,23 +28,23 @@ meal_allergyPattern = "(%s)" % '|'.join(allergy)
 
 class EducationOffice(Enum):
     UnKnown = ""
-    서울특별시 = "sen.go"
-    부산광역시 = "pen.go"
-    대구광역시 = "dge.go"
-    인천광역시 = "ice.go"
-    광주광역시 = "gen.go"
-    대전광역시 = "dje.go"
-    울산광역시 = "use.go"
-    세종특별자치시 = "sje.go"
-    경기도 = "goe.go"
-    강원도 = "kwe.go"
-    충청북도 = "cbe.go"
-    충청남도 = "cne.go"
-    전라북도 = "jbe.go"
-    전라남도 = "jne.go"
-    경상북도 = "gbe"
-    경상남도 = "gne.go"
-    제주특별자치도 = "jje.go"
+    q = "sen.go"
+    p = "pen.go"
+    o = "dge.go"
+    n = "ice.go"
+    m = "gen.go"
+    l = "dje.go"
+    k = "use.go"
+    j = "sje.go"
+    i = "goe.go"
+    h = "kwe.go"
+    g = "cbe.go"
+    f = "cne.go"
+    e = "jbe.go"
+    d = "jne.go"
+    c = "gbe"
+    b = "gne.go"
+    a = "jje.go"
 
 
 class MealType(Enum):
@@ -56,24 +56,24 @@ class MealType(Enum):
 
 class Allergy(Enum):
     Unknown = 0
-    난류 = 1
-    우유 = 2
-    메밀 = 3
-    땅콩 = 4
-    대두 = 5
-    밀 = 6
-    고등어 = 7
-    게 = 8
-    새우 = 9
-    돼지고기 = 10
-    복숭아 = 11
-    토마토 = 12
-    아황산류 = 13
-    호두 = 14
-    닭고기 = 15
-    쇠고기 = 16
-    오징어 = 17
-    조개류 = 18
+    r = 1
+    q = 2
+    p = 3
+    o = 4
+    n = 5
+    m = 6
+    l = 7
+    k = 8
+    j = 9
+    i = 10
+    h = 11
+    g = 12
+    f = 13
+    e = 14
+    d = 15
+    c = 16
+    b = 17
+    a = 18
 
     def fromString(str):
         idx = index(allergy, lambda item: item == str)

46c3b96edd815192c770d3a9fd424fa1f5fae543
devel <develope_e@naver.om>
2016-07-21 21:36:34 +0900

    heroku datetime test

--- utils/datetime.py
+++ utils/datetime.py
@@ -1,20 +1,19 @@
 # coding: utf-8
 
-from datetime import datetime
-import time
+#from datetime import datetime
+#import time
 #import pytz
 
 class datetimeEx:
     @staticmethod
     def now():
-        #tz = pytz.timezone('Asia/Seoul')  # <- put your local timezone here
-        #d = datetime.now(tz)  # the current time in your local timezone
-        #return datetimeEx.totimestamp(datetime(2016, 7, 16))
-        return time.time()
+        pass
+        #return time.time()
 
 
     @staticmethod
     def totimestamp(dt):
-        epoch = datetime(1970, 1, 1)
-        td = dt - epoch
-        return (td.microseconds + (td.seconds + td.days * 86400) * 10**6) / 10**6
+        pass
+        #epoch = datetime(1970, 1, 1)
+        #td = dt - epoch
+        #return (td.microseconds + (td.seconds + td.days * 86400) * 10**6) / 10**6

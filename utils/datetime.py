d62e8df4b910fcfb28208cb7507d916312a53e33
devel <develope_e@naver.om>
2016-07-21 22:35:12 +0900

    datetimeEx fix 8

--- utils/datetime.py
+++ utils/datetime.py
@@ -1,22 +1,16 @@
 # coding: utf-8
 
-#from datetime import datetime
-import datetime
+from datetime import datetime
 
 
 class datetimeEx:
     @staticmethod
     def now():
-        try:
-            return datetime.datetime.now()
-        except:
-            return "error"
-        #return datetimeEx.totimestamp(datetime.now())
+        return datetimeEx.totimestamp(datetime.now())
 
 
     @staticmethod
-    def toTimestamp(dt):
-        pass
-        #epoch = datetime(1970, 1, 1)
-        #td = dt - epoch
-        #return (td.microseconds + (td.seconds + td.days * 86400) * 10**6) / 10**6
+    def totimestamp(dt):
+        epoch = datetime(1970, 1, 1)
+        td = dt - epoch
+        return (td.microseconds + (td.seconds + td.days * 86400) * 10**6) / 10**6

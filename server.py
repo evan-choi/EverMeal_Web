2dbc64d010c7d838b7211b3653b2268e460efd3a
devel <develope_e@naver.om>
2016-07-21 19:30:15 +0900

    fix 2

--- server.py
+++ server.py
@@ -1,16 +1,11 @@
 # coding: utf-8
 
-from datetime import datetime
 from app import create_app
-import pytz
-
+from utils.datetime import datetimeEx
 
 if __name__ == '__main__':
     app = create_app()
-
-    tz = pytz.timezone('Asia/Seoul')  # <- put your local timezone here
-    now = datetime.now(tz)  # the current time in your local timezone
-
+    print(datetimeEx.now())
     app.run(host='127.0.0.1', port=8080)
 else:
     app = create_app()
--- utils/datetime.py
+++ utils/datetime.py
@@ -1,13 +1,20 @@
 # coding: utf-8
 
+from datetime import datetime
+import time
+#import pytz
+
 class datetimeEx:
     @staticmethod
     def now():
-        return 0#datetimeEx.totimestamp(datetime.now())
+        #tz = pytz.timezone('Asia/Seoul')  # <- put your local timezone here
+        #d = datetime.now(tz)  # the current time in your local timezone
+        #return datetimeEx.totimestamp(datetime(2016, 7, 16))
+        return time.time()
 
 
     @staticmethod
     def totimestamp(dt):
-        #epoch = datetime(1970, 1, 1)
-        #td = dt - epoch
-        return 0#(td.microseconds + (td.seconds + td.days * 86400) * 10**6) / 10**6
+        epoch = datetime(1970, 1, 1)
+        td = dt - epoch
+        return (td.microseconds + (td.seconds + td.days * 86400) * 10**6) / 10**6
